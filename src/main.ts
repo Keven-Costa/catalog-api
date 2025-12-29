46e577212fb1f5520d261961b7cc1222ee496500
Keven Costa <ogkeven278@gmail.com>
2025-12-28 23:46:54 -0300

    chore: bootstrap swagger documentation in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 4000);
+  const app = await NestFactory.create( AppModule );
+  const config = new DocumentBuilder()
+    .setTitle( 'CatalogAPI' )
+    .setDescription( 'Projeto catalogar' )
+    .setContact( "GitHub", "https://github.com/Keven-Costa", "...")
+    .setVersion( '1.0' )
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
+  process.env.TZ = '-03:00';
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+
+  await app.listen( 4000 );
 }
+
 bootstrap();
