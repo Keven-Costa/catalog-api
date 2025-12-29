db3e89f7d544d602087c337213e10fed7503abd2
Keven Costa <ogkeven278@gmail.com>
2025-12-28 22:26:13 -0300

    feat: initial ORM configuration and environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriaModule } from './categoria/categoria.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
-})
+  imports: [
+    ConfigModule.forRoot( {
+      isGlobal: true, 
+    } ),
+
+    TypeOrmModule.forRootAsync( {
+      imports: [ ConfigModule ],
+      inject: [ ConfigService ],
+      useFactory: ( configService: ConfigService ) => ( {
+        type: 'mysql', 
+        host: configService.get< string >( 'DB_HOST' ),
+        port: configService.get< number >( 'DB_PORT' ),
+        username: configService.get< string >( 'DB_USERNAME' ),
+        password: configService.get< string >( 'DB_PASSWORD' ),
+        database: configService.get< string >( 'DB_DATABASE' ),
+        autoLoadEntities: true,
+        synchronize: true, 
+      } ),
+    } ),
+
+    CategoriaModule,
+  ],
+} )
 export class AppModule {}
