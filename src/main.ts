11ac698759d49d5d273033070b4672479f39496d
Keven Costa <ogkeven278@gmail.com>
2026-01-01 20:10:16 -0300

    docs: setup global Swagger metadata and JWT security scheme

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,36 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create( AppModule );
   const config = new DocumentBuilder()
-    .setTitle( 'CatalogAPI' )
-    .setDescription( 'Projeto catalogar' )
-    .setContact( "GitHub", "https://github.com/Keven-Costa", "...")
-    .setVersion( '1.0' )
-    .addBearerAuth()
-    .build();
+      .setTitle('CatalogAPI')
+      .setDescription(
+        'API RESTful robusta para catalogação de bens e ativos. ' +
+        'Permite o controle completo de itens, categorias, localizações e histórico de auditoria.',
+      )
+      .setContact(
+        'Keven Costa', 
+        'https://github.com/Keven-Costa', 
+        'ogkeven278@gmail.com'
+      )
+      .setVersion('1.0.0')
+      .addTag('Item', 'Operações de gerenciamento de itens do catálogo')
+      .addTag('Categoria', 'Classificações dos itens')
+      .addTag('Localização', 'Gestão dos locais físicos onde os itens estão armazenados')
+      .addTag('Imagem', 'Gerenciamento de galeria de fotos ')
+      .addTag('Usuários', 'Controle de perfis, permissões e dados dos usuários do sistema')
+      .addTag('Logs', 'Histórico de auditoria e movimentações')
+      .addTag('Auth', 'Autenticação e controle de acesso')
+      .addBearerAuth(
+        {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+          name: 'JWT',
+          description: 'Insira o token JWT gerado no login',
+          in: 'header',
+        },
+        'access-token', 
+      )
+      .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/swagger', app, document);
