772c7928aa7e28ad28e2cf2a04d668dc408e7047
Keven Costa <ogkeven278@gmail.com>
2026-01-01 18:56:19 -0300

    refactor(item): integrate enhanced logging system and improve update logic

--- src/item/item.service.ts
+++ src/item/item.service.ts
@@ -5,6 +5,7 @@ import { CreateItemDto } from './dto/create-item.dto';
 import { UpdateItemDto } from './dto/update-item.dto';
 import { Item } from './entities/item.entity';
 import { Tag } from '../tag/entities/tag.entity'; 
+import { LogsService } from '../logs_/logs_.service';
 
 @Injectable()
 export class ItemService {
@@ -14,6 +15,8 @@ export class ItemService {
 
     @InjectRepository( Tag )
     private readonly tagRepository: Repository< Tag >,
+  
+    private readonly logService: LogsService
   ) {}
 
   async create( createItemDto: CreateItemDto ): Promise< Item > {
@@ -30,12 +33,22 @@ export class ItemService {
       } );
     }
 
-    return this.itemRepository.save( novoItem );
+    const itemSalvo = await this.itemRepository.save( novoItem );
+
+    const createLogDto = {
+      acao: 'Criar',
+      descricao_mudanca: "teste",
+      item_id: itemSalvo.id
+    }
+
+    this.logService.create( createLogDto )
+    
+    return itemSalvo
   }
 
   async findAll(): Promise< Item[] > {
     return this.itemRepository.find( {
-      relations: [ 'categoria', 'localizacao', 'tags', 'imagens' ],
+      relations: [ 'categoria', 'localizacao', 'tags', 'imagens', 'logs' ],
     } );
   }
 
@@ -52,22 +65,54 @@ export class ItemService {
     return item;
   }
 
-  async update( id: number, updateItemDto: UpdateItemDto ): Promise< Item > {
-    const { tag_ids, categoria_id, local_id, ...dadosRestantes } = updateItemDto;
-    const item = await this.findOneById( id );
-    this.itemRepository.merge( item, dadosRestantes );
+async findOneByName( nome: string ): Promise< Item > {
+  const item = await this.itemRepository.findOne( {
+    where: { nome: nome },
+    relations: ['categoria', 'localizacao', 'tags', 'imagens', 'logs']
+  } );
 
-    if ( categoria_id ) item.categoria = { id: categoria_id } as any;
-    if ( local_id ) item.localizacao = { id: local_id } as any;
-    if ( tag_ids ) {
-      item.tags = await this.tagRepository.findBy( { id: In( tag_ids ) } );
-    }
-
-    return this.itemRepository.save( item );
+  if ( !item ) {
+    throw new NotFoundException( `Item com o nome "${ nome }" não encontrado` );
   }
+
+  return item;
+}
+
+
+async update ( id: number, updateItemDto: UpdateItemDto ): Promise< Item > {
+  const { tag_ids, categoria_id, local_id, ...dadosRestantes } = updateItemDto;
+  const itemAntigo = await this.findOneById( id );
+  const copiaAntigo = { ...itemAntigo };
+
+  this.itemRepository.merge( itemAntigo, dadosRestantes) ;
+
+  if ( categoria_id ) itemAntigo.categoria = { id: categoria_id } as any;
+  if ( local_id ) itemAntigo.localizacao = { id: local_id } as any;
+  if ( tag_ids ) {
+    itemAntigo.tags = await this.tagRepository.findBy( { id: In( tag_ids ) } );
+  }
+
+  const itemAtualizado = await this.itemRepository.save( itemAntigo );
+  const itemNovoCompleto = await this.findOneById( id );
+  const descricao = await this.logService.createDescription( copiaAntigo, itemNovoCompleto );
+
+  await this.logService.create( {
+    acao: 'Atualizar',
+    descricao_mudanca: descricao,
+    item_id: itemAtualizado.id
+  } );
+
+  return itemAtualizado;
+}
 
   async remove( id: number ): Promise< void > {
     const item = await this.findOneById( id );
-    await this.itemRepository.remove( item );
+    
+    await this.logService.create( {
+      acao: 'Excluir',
+      item_id: item.id
+    } );
+
+    await this.itemRepository.softRemove( item );
   }
 }
